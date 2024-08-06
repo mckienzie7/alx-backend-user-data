7ffacdb4d43513371316cdc7e4044ba5f9c873e8
Michael Solomon <Michaelelsa12@gmail.com>
2024-08-06 12:40:09 +0300

    basic_auth authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
-"""Basic Auth class created"""
-
-from auth.auth import Auth
+"""A class that inherits from Auth class"""
+from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
 
+    def extract_base64_autherization_header(self, authorization_header : str) -> str:
+        
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header,  str):
+            return None
+
+        if not authorization_header.startwith("Basic "):
+            return None
+
+        return authorization_header[6:]
