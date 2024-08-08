d5133a01af3f2a9528f8caee7769dcb4391a5957
Michael Solomon <Michaelelsa12@gmail.com>
2024-08-08 11:42:39 +0300

    session_auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,49 +1,34 @@
+#!/usr/bin/env python3
+"""
+    - SessionAuth creation
+"""
 from api.v1.auth.auth import Auth
+import os
 import uuid
 
 
 class SessionAuth(Auth):
-    
+    """
+        - Session!uth class
+    """
+
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
 
-        if user_id is None:
-            return None
-
-        if not isinstance(user_id, str):
+        if user_id is None and not isinstance(user_id, str):
             return None
 
         session_id = str(uuid.uuid4())
-
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
 
-    def user_id_for_session_id(Self, session_id:str = None) -> str:
+    def user_id_for_session_id(self, session_id: str = None) -> str:
 
-        if session_id is None:
+        if session_id is None and not isinstance(session_id, str):
             return None
 
-        if not isinstance(session_id, str):
-            return None
+        user_id = self.user_id_by_session_id.get(session_id)
 
-        return Self.user_id_by_session_id.get(session_id)
-
-
-    def current_user(self, request=None):
-
-        """return user based on session id and user_id"""
-        session_id = self.session_cookie(request)
-
-        if session_id is not None:
-            user_id = self.user_id_for_session_id(session_id)
-            if user_id is not None:
-
-                user = User.get(user_id)
-
-                return user
-
-            return None
-
-        return None
+        return user_id
