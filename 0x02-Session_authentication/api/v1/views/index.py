80313555304e27861a044e27c661e0a6082bade4
Michael Solomon <michaelelsa12@gmail.com>
2024-08-08 12:11:34 +0300

    update seesion dta

---
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" Module of Index views
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False, methods=['GET'])
+def unauthorized() -> str:
+    """ Add a new error handler for this
+    status code, the response must be
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False, methods=['GET'])
+def forbidden() -> str:
+    """ Forbidden route
+    """
+    abort(403)
