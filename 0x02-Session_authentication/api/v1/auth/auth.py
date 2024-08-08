a8f66cc422f04ca5fa3d579ebe4292b40007f626
Michael Solomon <Michaelelsa12@gmail.com>
2024-08-08 11:54:13 +0300

    upd

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ from flask import request
 from typing import List, TypeVar
 import os
 
+
 class Auth:
     """Class to manage api authentications"""
 
@@ -42,13 +43,3 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """current user"""
         return None
-
-    def session_cookie(self, request=None):
-
-        if request is None:
-            return None
-
-
-        cookie_name = os.getenv("SESSION_NAME")
-
-        return request.cookies.get(cookie_name)
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,32 +1,34 @@
-#!/usr/bin/env python3
-"""
-    - SessionAuth creation
-"""
+i#!/usr/bin/env python3
+"""A classs SessionAuth that inherits from AUTH"""
 from api.v1.auth.auth import Auth
-import os
-import uuid
+from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
-    """
-        - Session!uth class
-    """
-
+    """Session auth class with a class attribute"""
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-
-        if user_id is None and not isinstance(user_id, str):
+        """creates a session id for a user"""
+        if user_id is None:
             return None
 
-        session_id = str(uuid.uuid4())
+        if not isinstance(user_id, str):
+            return None
+
+        session_id: str = str(uuid4())
+
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
+        """eturn user id based on session id"""
+        if session_id is None:
+            return None
 
-        if session_id is None and not isinstance(session_id, str):
+        if not isinstance(session_id, str):
             return None
 
         user_id = self.user_id_by_session_id.get(session_id)
