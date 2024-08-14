22eb9b324cae1ca1988d306ddbec4a04e8a44c8c
Michael Solomon <Michaelelsa12@gmail.com>
2024-08-14 16:05:48 +0300

    update user_auth

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -12,10 +12,12 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'users'
+    """ User class inherit from Base """
+
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
