f67ffdb8acd4698e8a3872389c02506c511c6557
Michael Solomon <Michaelelsa12@gmail.com>
2024-08-15 22:54:58 +0300

    final upd

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,22 +1,109 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-from auth import Auth
+"""End-to-end integration test for user authentication service."""
+import requests
 
-email = 'me@me.com'
-password = 'mySecuredPwd'
+BASE_URL = "http://0.0.0.0:5000"
 
-auth = Auth()
 
-try:
-    user = auth.register_user(email, password)
-    print("successfully created a new user!")
-except ValueError as err:
-    print("could not create a new user: {}".format(err))
+def register_user(email: str, password: str) -> None:
+    """Register a user."""
+    url = f"{BASE_URL}/users"
+    payload = {
+        'email': email,
+        'password': password
+    }
+    response = requests.post(url, data=payload)
+    assert response.status_code == 200
+    assert response.json() == {"email": email, "message": "user created"}
 
-try:
-    user = auth.register_user(email, password)
-    print("successfully created a new user!")
-except ValueError as err:
-    print("could not create a new user: {}".format(err))
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """Log in with a wrong password."""
+    url = f"{BASE_URL}/sessions"
+    payload = {
+        'email': email,
+        'password': password
+    }
+    response = requests.post(url, data=payload)
+    assert response.status_code == 401
+
+
+def log_in(email: str, password: str) -> str:
+    """Log in and return session ID."""
+    url = f"{BASE_URL}/sessions"
+    payload = {
+        'email': email,
+        'password': password
+    }
+    response = requests.post(url, data=payload)
+    assert response.status_code == 200
+    assert response.json() == {"email": email, "message": "logged in"}
+    return response.cookies.get('session_id')
+
+
+def profile_unlogged() -> None:
+    """Retrieve profile information while logged out."""
+    url = f"{BASE_URL}/profile"
+    response = requests.get(url)
+    assert response.status_code == 403
+
+
+def profile_logged(session_id: str) -> None:
+    """Retrieve profile information while logged in."""
+    url = f"{BASE_URL}/profile"
+    cookies = {'session_id': session_id}
+    response = requests.get(url, cookies=cookies)
+    assert response.status_code == 200
+    assert "email" in response.json()
+
+
+def log_out(session_id: str) -> None:
+    """Log out of a session."""
+    url = f"{BASE_URL}/sessions"
+    cookies = {'session_id': session_id}
+    response = requests.delete(url, cookies=cookies)
+    assert response.status_code == 200
+    assert response.json() == {"message": "Bienvenue"}
+
+
+def reset_password_token(email: str) -> str:
+    """Request a password reset and return reset token."""
+    url = f"{BASE_URL}/reset_password"
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+    assert response.status_code == 200
+    assert "email" in response.json()
+    assert response.json()["email"] == email
+    assert "reset_token" in response.json()
+    return response.json().get('reset_token')
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """Update user's password."""
+    url = f"{BASE_URL}/reset_password"
+    payload = {
+        'email': email,
+        'reset_token': reset_token,
+        'new_password': new_password
+    }
+    response = requests.put(url, data=payload)
+    assert response.status_code == 200
+    assert response.json() == {"email": email, "message": "Password updated"}
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+    """run the codes"""
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
